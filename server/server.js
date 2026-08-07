3221a8b481c882aef4b52cba7a64a7fb54368ef7
CodeWith-Het <hetp1065@gmail.com>
2026-08-07 16:01:19 -0500

    done

--- server/server.js
+++ server/server.js
@@ -52,7 +52,7 @@ mongoose
 
   app.use(express.static(path.join(__dirname, "public")));
 
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
   });
 
